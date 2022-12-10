73584ada68ba084ffc0d70ac24c5046ca49cd1d2
ykerit <ykerforit@gmail.com>
2022-12-10 11:46:37 +0800

    change dep

--- view/src/main.rs
+++ view/src/main.rs
@@ -1,56 +1,30 @@
-use log::debug;
-use simple_logger::SimpleLogger;
 use winit::{
-    dpi::LogicalSize,
-    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
-    event_loop::EventLoop,
-    window::WindowBuilder,
+    dpi::{LogicalSize, PhysicalSize},
+    event::{DeviceEvent, ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{DeviceEventFilter, EventLoop},
+    window::{Fullscreen, WindowBuilder},
 };
 
 fn main() {
-    SimpleLogger::new().init().unwrap();
     let event_loop = EventLoop::new();
 
     let window = WindowBuilder::new()
         .with_title("A fantastic window!")
-        .with_inner_size(LogicalSize::new(128.0, 128.0))
-        .with_resize_increments(LogicalSize::new(25.0, 25.0))
+        .with_inner_size(LogicalSize::new(960.0, 680.0))
         .build(&event_loop)
         .unwrap();
 
-    let mut has_increments = true;
+    event_loop.set_device_event_filter(DeviceEventFilter::Never);
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_wait();
 
         match event {
+            Event::RedrawRequested(Window_id) => {}
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
             } if window_id == window.id() => control_flow.set_exit(),
-            Event::WindowEvent {
-                event:
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                virtual_keycode: Some(VirtualKeyCode::Space),
-                                state: ElementState::Released,
-                                ..
-                            },
-                        ..
-                    },
-                window_id,
-            } if window_id == window.id() => {
-                has_increments = !has_increments;
-
-                let new_increments = match window.resize_increments() {
-                    Some(_) => None,
-                    None => Some(LogicalSize::new(25.0, 25.0)),
-                };
-                debug!("Had increments: {}", new_increments.is_none());
-                window.set_resize_increments(new_increments);
-            }
-            Event::MainEventsCleared => window.request_redraw(),
             _ => (),
         }
     });
